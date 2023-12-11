8448e0859229da33b79f6a771817d0d38e813ac7
vipinjarwalpython <vipinjarwalpydev@gmail.com>
2023-12-11 11:51:10 +0530

    site rendered

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -16,7 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from ecommerce import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("", views.index, name="index"),
+    path("shop/", views.shop, name="shop"),
+    path("about/", views.aboutus, name="aboutus"),
+    path("services/", views.services, name="services"),
+    path("blog/", views.blog, name="blog"),
+    path("contact/", views.contactus, name="contactus"),
+    # path("admin/", admin.site.urls),
 ]
